582b6c9fa3fca06983c2c70ba2bf6e3a0ec326e8
David iMac <davicho_balla@hotmail.com>
2019-08-31 19:55:22 -0400

    Challenge saving and ignoring dsStore

---
+++ Section8/MinElement.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinElement{
+   
+   private static Scanner scan = new Scanner(System.in);
+   public static void main(String[] args) {
+      int[] myArray = readNums(3);
+
+      System.out.println("Your values are " + Arrays.toString(myArray));
+
+      System.out.println("The minimum value in your array is: " + findMin(myArray)); 
+   }
+
+   public static int[] readNums(int count){
+      System.out.println("Please enter " + count + " values\r");
+      int[] array = new int[count];
+
+      for (int i = 0; i < array.length; i++) {
+         array[i] = scan.nextInt();
+      }
+      return array;
+   }
+
+   public static int findMin(int[] array){
+      //System.out.println("The copy of the array is: " + Arrays.toString(min));     // TODO: this line is not neccesary, i was just testing.
+      int min = 0;
+      boolean flag = true;
+      while (flag) {
+         flag = false;
+         for (int i = 0; i < array.length-1; i++) {
+            if (array[i]<array[i+1]) {
+               min = array[i];
+            } else{
+               min = array[i+1];
+            } 
+         }
+      }
+      return min;
+   }
+}
