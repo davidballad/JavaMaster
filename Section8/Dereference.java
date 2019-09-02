49619d417049220e20d41b9d1ef57ba4071060f6
davidballad <42699992+davidballad@users.noreply.github.com>
2019-09-02 14:34:06 -0400

    commentchange

--- Section8/Dereference.java
+++ Section8/Dereference.java
@@ -9,16 +9,32 @@ public class Dereference{
     System.out.println("myArray values: " + Arrays.toString(myArray));
     System.out.println("yourArray values: " + Arrays.toString(yourArray));
 
-    yourArray = new int[] {6, 6, 6};      //basically creating a new array
+    yourArray = new int[] {6, 6, 6};      //basically creating a new array because we cannot dereference an array
     dereference(yourArray);
     System.out.println("deref myArray values: " + Arrays.toString(myArray));
-    System.out.println("deref yourArray values: " + Arrays.toString(yourArray));
+    System.out.println("deref yourArray values: " + Arrays.toString(dereference(yourArray)));
 
+
+          int a = 16;
+          int b = a;
+          System.out.println("A is: " + a);
+          System.out.println("B is: " + b);
+
+
+
+          System.out.println("A in deref is: " + a);
+          System.out.println("B in deref is: " + derefNum(b));
   }
 
-  private static void dereference(int[] array){     //you can deferentiate an array
+  private static int[] dereference(int[] array){     //you can NOT deferentiate an array. CHANGE TO VOID
 
     array[0] = 3;
+    return array;
   }
+
+          private static int derefNum(int num){      // Not fully understood
+            num += 8;
+            return num;
+          }
 
 }
